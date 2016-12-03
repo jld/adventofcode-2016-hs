5a6589e2772f42bd08421549ee21413a99d2afbb
Jed Davis <jld@xlerb.net>
2016-12-03 11:04:49 -0700

    Day 2: Redo for part 2. Infinite lists everywhere. Probably leaks memory like a stuck pig.

--- day2/KeyPad.hs
+++ day2/KeyPad.hs
@@ -1,23 +1,7 @@
 module KeyPad where
 
-newtype Sat3 = Sat3 Int
-             deriving (Eq, Ord, Show)
-inc (Sat3 n) = Sat3 (min 2 (n+1))
-dec (Sat3 n) = Sat3 (max 0 (n-1))
-
-data Button = Button Sat3 Sat3
-            deriving (Eq, Show)
-
-button_val (Button (Sat3 x) (Sat3 y)) = 1 + x + 3 * y
-five = Button (Sat3 1) (Sat3 1)
-
 data Dir = Up | Dn | Lf | Rt
          deriving (Eq, Show)
-move Up (Button x y) = Button x (dec y)
-move Dn (Button x y) = Button x (inc y)
-move Lf (Button x y) = Button (dec x) y
-move Rt (Button x y) = Button (inc x) y
-
 
 parse_dir 'U' = Up
 parse_dir 'D' = Dn
@@ -28,11 +12,43 @@ parse_dirs = map parse_dir
 parse_prob = map parse_dirs . words
 parse_file path = fmap parse_prob $ readFile path
 
-moves :: [Dir] -> Button -> Button
+data Zipper a = Zipper [a] a [a]
+fwd (Zipper before here after) = Zipper (here:before) (head after) (tail after)
+back (Zipper before here after) = Zipper (tail before) (head before) (here:after)
+zget (Zipper _ here _) = here
+
+instance Functor Zipper where
+  fmap f (Zipper before here after) = Zipper (map f before) (f here) (map f after)
+
+mkzip fill before here after = Zipper (reverse before ++ repeat fill) here (after ++ repeat fill)
+mkline = mkzip ' '
+blankline = mkline "" ' ' ""
+mkpad = mkzip blankline
+
+type Pad = Zipper (Zipper Char)
+padget = zget . zget
+shift Up = back
+shift Dn = fwd
+shift Lf = fmap back
+shift Rt = fmap fwd
+move dir pad =
+  if padget newpad == ' ' then pad else newpad
+  where newpad = shift dir pad
+
+key9 = (mkpad
+        [mkline "1" '2' "3"]
+        (mkline "4" '5' "6")
+        [mkline "7" '8' "9"])
+
+key13 = (mkpad
+         [mkline "" ' ' " 1",
+          mkline "" ' ' "234"]
+         (mkline "" '5' "6789")
+         [mkline "" ' ' "ABC",
+          mkline "" ' ' " D"])
+
+moves :: [Dir] -> Pad -> Pad
 moves = flip (foldl (flip move))
 
-lsolve1 :: [[Dir]] -> [Int]
-lsolve1 = map button_val . tail . scanl (flip moves) five
-
-solve1 :: [[Dir]] -> String
-solve1 = concat . map show . lsolve1
+solve :: Pad -> [[Dir]] -> String
+solve pad = map padget . tail . scanl (flip moves) pad
