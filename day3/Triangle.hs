1b139aa981e9449834530e7651d7913f8133e6fd
Jed Davis <jld@xlerb.net>
2016-12-03 11:28:04 -0700

    Day 3: part 2 was surprisingly low-drama.

--- day3/Triangle.hs
+++ day3/Triangle.hs
@@ -1,5 +1,5 @@
 module Triangle where
-import Data.List (sort)
+import Data.List (sort, transpose)
 
 data Tri = Tri Integer Integer Integer
            deriving (Eq, Show)
@@ -9,10 +9,20 @@ tri_of_list l =
    [a, b, c] -> Tri a b c
    _ -> error ("tri_of_list: Length " ++ (show $ length l) ++ " /= 3")
 
-parse_tri = tri_of_list . map read . words
+tri_of_words = tri_of_list . map read
+parse_tri = tri_of_words . words
 parse_tris = map parse_tri . lines
-parse_file = fmap parse_tris . readFile 
+fileize f = fmap f . readFile
+parse_file = fileize parse_tris
 
 is_valid (Tri a b c) = (a + b) > c
 
 solve = length . filter is_valid
+
+explode = map words . lines
+mangle (a:b:c:ds) = [a,b,c]:(mangle ds)
+mangle [] = []
+twirl = concat . map transpose . mangle
+
+altparse_tris = map tri_of_words . twirl . explode
+altparse_file = fileize altparse_tris
