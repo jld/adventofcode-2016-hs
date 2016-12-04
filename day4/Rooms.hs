e8ef44d3d2101c5c804a5b39ad642a049431bf25
Jed Davis <jld@xlerb.net>
2016-12-03 22:33:15 -0700

    Day 4: refactor slightly.

--- day4/Rooms.hs
+++ day4/Rooms.hs
@@ -2,8 +2,8 @@ module Rooms where
 import Data.Char (isDigit, isLower)
 import Data.List (sort, group)
 
-namehash :: String -> String
-namehash = map snd . take 5 . sort . map (\l -> (- length l, head l)) . group . sort
+namehash =
+  map snd . take 5 . sort . map (\l -> (- length l, head l)) . group . sort . filter (/= '-')
 
 data Room = Room String Integer String
             deriving (Eq, Show)
@@ -11,7 +11,7 @@ data Room = Room String Integer String
 roomsplit s =
   let (enc, rest) = span (\c -> c == '-' || isLower c) s in
   let (num, brac) = span isDigit rest in
-  Room (filter (/= '-') enc) (read num) brac
+  Room enc (read num) brac
 
 room_num (Room _ num _) = num
 is_valid (Room enc _ brac) = brac == ("[" ++ (namehash enc) ++ "]")
