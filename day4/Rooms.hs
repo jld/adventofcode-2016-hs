d46d56f171eedd95723731749c83603c53f8c3ac
Jed Davis <jld@xlerb.net>
2016-12-03 22:31:37 -0700

    Day 4: part 1; such function, very compose, wow.

---
+++ day4/Rooms.hs
@@ -0,0 +1,22 @@
+module Rooms where
+import Data.Char (isDigit, isLower)
+import Data.List (sort, group)
+
+namehash :: String -> String
+namehash = map snd . take 5 . sort . map (\l -> (- length l, head l)) . group . sort
+
+data Room = Room String Integer String
+            deriving (Eq, Show)
+
+roomsplit s =
+  let (enc, rest) = span (\c -> c == '-' || isLower c) s in
+  let (num, brac) = span isDigit rest in
+  Room (filter (/= '-') enc) (read num) brac
+
+room_num (Room _ num _) = num
+is_valid (Room enc _ brac) = brac == ("[" ++ (namehash enc) ++ "]")
+
+parse_prob = map roomsplit . lines
+parse_file = fmap parse_prob . readFile
+
+solve = sum . map room_num . filter is_valid
