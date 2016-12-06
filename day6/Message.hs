9ce3fbeda387d2952452dd5da2e5fb323c6e9525
Jed Davis <jld@xlerb.net>
2016-12-06 00:04:42 -0700

    Day 6: Part 2... maybe I'll just copypaste.

--- day6/Message.hs
+++ day6/Message.hs
@@ -3,3 +3,6 @@ import Data.List (sort, transpose, group)
 
 mostcommon = snd . head . sort . map (\l -> (- length l, head l)) . group . sort
 solve = map mostcommon . transpose . lines
+
+leastcommon = snd . head . sort . map (\l -> (length l, head l)) . group . sort
+altsolve = map leastcommon . transpose . lines
