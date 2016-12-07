ef357e954c8b51710a5663cc87a2db1676239756
Jed Davis <jld@xlerb.net>
2016-12-07 01:36:25 -0700

    Day 7: part 2 is inspired by part 1 but mostly separate.

--- day7/Snoop.hs
+++ day7/Snoop.hs
@@ -1,4 +1,6 @@
 module Snoop where
+import Data.Tuple (swap)
+import qualified Data.Set as S
 
 -- FIXME: there's probably a nicer way to pattern-match this
 has_abba (a:b:c:d:es) = (a == d && b == c && a /= b) || has_abba (b:c:d:es)
@@ -17,3 +19,19 @@ snoopable (sns,hns) = any has_abba sns && not (any has_abba hns)
 brunzip = unzip . unbracket
 check = snoopable . brunzip
 solve = length . filter check . lines
+
+-- FIXME: again, there's probably prettier pattern-matching
+abas (a:b:c:ds) = thing ++ abas (b:c:ds)
+  where thing = if a == c && a /= b then [(a, b)] else []
+abas _ = []
+babs = map swap . abas
+
+setify = foldr S.insert S.empty
+
+-- FIXME: surely this is factorable
+listens (sns,hns) = S.intersection aba_set bab_set
+  where aba_set = setify $ concat $ map abas sns
+        bab_set = setify $ concat $ map babs hns
+listenable = not . S.null . listens
+altcheck = listenable . brunzip
+altsolve = length . filter altcheck . lines
