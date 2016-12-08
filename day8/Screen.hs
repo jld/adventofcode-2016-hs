dd64817ec93e4ceec7d1bf9cb2942f2eb75f098d
Jed Davis <jld@xlerb.net>
2016-12-08 09:17:58 -0700

    Day 8: the rendering engine; still needs a parser.
    Lots of type annotations because it's too early in the morning.

---
+++ day8/Screen.hs
@@ -0,0 +1,44 @@
+module Screen where
+
+type Dim = Int
+scrw :: Dim
+scrw = 50
+scrh :: Dim
+scrh = 6
+
+type Coord = (Dim, Dim)
+type Pic = Coord -> Bool
+
+blank :: Pic
+blank = const False
+
+rect :: Dim -> Dim -> Pic
+rect w h (x,y) = x < w && y < h
+
+over :: Pic -> Pic -> Pic
+over upper lower xy = upper xy || lower xy
+
+rect_over w h = over $ rect w h
+
+rot_left :: Dim -> Dim -> Coord -> Coord
+rot_left row dx (x,y) = (if y == row then (x - dx) `mod` scrw else x, y)
+
+rotright_over :: Dim -> Dim -> Pic -> Pic
+rotright_over row dx f = f . rot_left row dx
+
+rot_up :: Dim -> Dim -> Coord -> Coord
+rot_up col dy (x,y) = (x, if x == col then (y - dy) `mod` scrh else y)
+
+rotdown_over :: Dim -> Dim -> Pic -> Pic
+rotdown_over col dy f = f . rot_up col dy
+
+renderb :: Pic -> [[Bool]]
+renderb f = [[f (x,y) | x <- [0..pred scrw]] | y <- [0..pred scrh]]
+
+render :: Pic -> [[Char]]
+render = map (map (\b -> if b then '#' else '.')) . renderb
+
+display = putStr . unlines . render
+
+litcount :: Pic -> Int
+litcount = length . filter id . concat . renderb
