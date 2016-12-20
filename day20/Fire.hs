33d62e0ac42632778e8da04666729cb1df100c22
Jed Davis <jld@xlerb.net>
2016-12-19 22:49:37 -0700

    Day 20: Part 1; nothing fancy to see here.

---
+++ day20/Fire.hs
@@ -0,0 +1,31 @@
+module Fire where
+import Data.Word
+import Data.List
+
+type Addr = Word32
+data Range = Range { rangeMin :: Addr, rangeMax :: Addr }
+             deriving (Eq, Ord, Show)
+
+-- Assumes arguments in order.
+rmerge1 (Range a b) (Range c d)
+  | b >= c || succ b == c = Just $ Range a (b `max` d)
+  | otherwise             = Nothing
+
+-- Likewise.
+rmerge :: [Range] -> [Range]
+rmerge (r0:r1:rs)
+  | Just r <- rmerge1 r0 r1 = rmerge (r:rs)
+  | otherwise              = r0:(rmerge (r1:rs))
+rmerge rs = rs
+
+simplify = rmerge . sort
+
+solve' = succ . rangeMax . head
+solve = solve' . simplify
+
+parse_range s = Range a b
+  where [a,b] = map read $ words $ map tr s
+        tr '-' = ' '
+        tr c   = c
+parse_prob = map parse_range . lines
+parse_file = fmap parse_prob . readFile
