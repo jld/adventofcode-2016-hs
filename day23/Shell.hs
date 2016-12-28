f1ee693e5f5c3eebf8f47a8b76b73b681b6d3893
Jed Davis <jld@xlerb.net>
2016-12-27 20:28:28 -0700

    Day 23: Factor out some repetition.
    (BTW this isn't really fast enough to brute-force part 2; I'd estimate
    15-20 minutes with everything compiled -O2.  Fortunately, as the problem
    text hints, it's possible to make an educated guess.)
    
    (Speaking of lateral problem solving, I did Day 22 completely by hand,
    with a little help from some throwaway shell/awk one-liners I didn't
    bother to save, if you were wondering why that directory is missing.)

--- day23/Shell.hs
+++ day23/Shell.hs
@@ -8,20 +8,17 @@ import qualified Data.Vector.Unboxed.Mutable as VUM
 day12 c0 prog =
   launch $ do
     regWrite (Reg 2) c0
-    res <- whileOk $ step prog
-    case res of
-     Halt -> regRead (Reg 0)
-     _ -> do
-       pc <- getPC
-       error $ "Bunny Fault: " ++ show res ++ " at PC " ++ show pc
+    run $ step prog
 
 day23 a0 prog0 =
   launch $ do
     regWrite (Reg 0) a0
     prog <- VU.thaw prog0
-    res <- whileOk $ stepMut prog
-    case res of
-     Halt -> regRead (Reg 0)
-     _ -> do
-       pc <- getPC
-       error $ "Bunny Fault: " ++ show res ++ " at PC " ++ show pc
+    run $ stepMut prog
+
+run tick = whileOk tick >>= final
+
+final Halt = regRead (Reg 0)
+final res = do
+  pc <- getPC
+  error $ "Bunny Fault: " ++ show res ++ " at PC " ++ show pc
